d2358d70818b162502e8d0d946bf99286e0c7b8a
Bindu Parvathy <BinduParvathy@Deltek.com>
2025-07-01 20:08:22 +0800

    My first commit

--- tests/uitest.spec.ts
+++ tests/uitest.spec.ts
@@ -14,7 +14,7 @@ test.beforeAll('Running before all tests', () => {
         testData = stageTestData;
     }
 })
-
+ 
 // Write a test
 test('UI automation test using playwright', async ({ page }) => {
 
